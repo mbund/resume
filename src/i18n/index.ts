1d3687f46411be1614405ae8bf9599b0a5fcc5a8
Mark Bundschuh <bundschuhmark@gmail.com>
2023-08-07 22:04:49 -0400

    smarter i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,6 +4,8 @@ import { de } from "./de";
 export const languages = ["en", "de"] as const;
 export type Language = (typeof languages)[number];
 
+export const defaultLanguage: Language = "en";
+
 export type Schema = {
   name: string;
   about: string;
